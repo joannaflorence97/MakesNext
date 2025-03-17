4e782be7f7caa62e8f7ae366a2b718169e743a83
James Van Hinsbergh <jvanhins@gmail.com>
2025-03-17 21:03:15 +0000

    Add image carousel draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { getAllPosts } from '@/lib/posts';
 import { PostFilter } from '@/components/PostFilter';
+import { ImageCarousel } from '@/components/ImageCarousel';
 
 export default async function Home() {
   const posts = await getAllPosts();
 
   return (
-    <PostFilter
-      categories={['amelia', 'dress', 'lyra', 'joni']}
-      posts={posts}
-    />
+    <>
+      <ImageCarousel />
+      <PostFilter
+        categories={['amelia', 'dress', 'lyra', 'joni']}
+        posts={posts}
+      />
+    </>
   );
 }
