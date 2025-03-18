3079d1adaf607e5d491dbef332f285e6ecf6351c
James Van Hinsbergh <jvanhins@gmail.com>
2025-03-18 18:06:42 +0000

    Fix font loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import { Footer } from '@/components/Footer';
 
-const poppins = Poppins({ weight: ['300', '400', '500', '600', '700'] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
 
 export const metadata: Metadata = {
   title: {
